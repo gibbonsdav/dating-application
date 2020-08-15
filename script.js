7a8d4776a19bf6340c1fd17ace90cb14fb790fd6
David Gibbons <gibbonsdav@gmail.com>
2020-08-15 12:13:31 -0600

    finished

--- script.js
+++ script.js
@@ -29,6 +29,10 @@ function showError(input, message) {
   formControl.className = "form-control error"
   const small = formControl.querySelector("small")
   small.innerText = message
+  console.log(formControl.className)
+  if (formControl.className) {
+    modal.classList.remove("show-modal")
+  }
 }
 
 // Show success outline
@@ -42,43 +46,6 @@ function isValidEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   return re.test(String(email).toLowerCase())
 }
-
-// Check required fields
-// function checkRequired(inputArr) {
-//   let isRequired = false
-//   inputArr.forEach(function (input) {
-//     if (input.value.trim() === "") {
-//       showError(input, `${getFieldName(input)} is required`)
-//       isRequired = true
-//     } else {
-//       showSuccess(input)
-//     }
-//   })
-
-//   return isRequired
-// }
-
-// Check input length
-// function checkLength(input, min, max) {
-//   if (input.value.length < min) {
-//     showError(
-//       input,
-//       `${getFieldName(input)} must be at least ${min} characters`
-//     )
-//   } else if (input.value.length > max) {
-//     showError(
-//       input,
-//       `${getFieldName(input)} must be less than ${max} characters`
-//     )
-//   } else {
-//     showSuccess(input)
-//   }
-// }
-
-// Get fieldname
-// function getFieldName(input) {
-//   return input.id.charAt(0).toUpperCase() + input.id.slice(1)
-// }
 
 // Event listeners
 form.addEventListener("submit", function (e) {
